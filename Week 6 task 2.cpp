811281dade6b5937572fd32cde0b4b81605201f0
josk4901-dotcom <josk4901@gmail.com>
2026-06-14 19:20:52 +0300

    Add files via upload

---
+++ Week 6 task 2.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Student {
+private:
+    string name;
+    string admissionNumber;
+    double feeBalance;
+
+public:
+    void inputStudent() {
+        cout << "Enter student name: ";
+        getline(cin, name);
+
+        cout << "Enter admission number: ";
+        getline(cin, admissionNumber);
+
+        cout << "Enter fee balance: ";
+        cin >> feeBalance;
+    }
+
+    void makePayment() {
+        double amount;
+
+        cout << "Enter payment amount: ";
+        cin >> amount;
+
+        feeBalance -= amount;
+
+        if (feeBalance < 0)
+            feeBalance = 0;
+
+        cout << "Payment successful.\n";
+    }
+
+    void displayStatus() {
+        cout << "\nStudent Details\n";
+        cout << "Name: " << name << endl;
+        cout << "Admission Number: " << admissionNumber << endl;
+        cout << "Remaining Fee Balance: " << feeBalance << endl;
+    }
+};
+
+int main() {
+    Student s;
+
+    s.inputStudent();
+    s.displayStatus();
+
+    s.makePayment();
+
+    cout << "\nUpdated Fee Status:\n";
+    s.displayStatus();
+
+    return 0;
+}
