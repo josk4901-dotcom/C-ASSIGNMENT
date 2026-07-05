623468ac46811fcc558c9163d8fb3cf47cc68676
josk4901-dotcom <josk4901@gmail.com>
2026-07-05 21:57:58 +0300

    Add files via upload

---
+++ week 9 task 2.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Vehicle {
+protected:
+    string brand;
+    int year;
+
+public:
+    void inputVehicle() {
+        cout << "Enter brand: ";
+        getline(cin, brand);
+
+        cout << "Enter year of manufacture: ";
+        cin >> year;
+    }
+
+    void displayVehicle() {
+        cout << "Brand: " << brand << endl;
+        cout << "Year of Manufacture: " << year << endl;
+    }
+};
+
+class Car : public Vehicle {
+private:
+    int doors;
+    float engineCapacity;
+
+public:
+    void inputCar() {
+        inputVehicle();
+
+        cout << "Enter number of doors: ";
+        cin >> doors;
+
+        cout << "Enter engine capacity: ";
+        cin >> engineCapacity;
+    }
+
+    void displayCar() {
+        displayVehicle();
+        cout << "Number of Doors: " << doors << endl;
+        cout << "Engine Capacity: " << engineCapacity << " L" << endl;
+    }
+};
+
+int main() {
+    Car c;
+
+    c.inputCar();
+
+    cout << "\nCar Details\n";
+    c.displayCar();
+
+    return 0;
+}
