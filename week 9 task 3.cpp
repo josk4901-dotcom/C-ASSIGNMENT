114b1226abded67dabbed3de1c96d6d6393f1a4b
josk4901-dotcom <josk4901@gmail.com>
2026-07-06 17:47:42 +0300

    Add files via upload

---
+++ week 9 task 3.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Base class
+class Person
+{
+protected:
+    string name;
+
+public:
+    Person(string n = "")
+    {
+        name = n;
+    }
+
+    void setName(string n)
+    {
+        name = n;
+    }
+
+    string getName()
+    {
+        return name;
+    }
+};
+
+// Derived class
+class LibraryMember : public Person
+{
+private:
+    int memberID;
+    int booksBorrowed;
+
+public:
+    LibraryMember(string n, int id, int books)
+        : Person(n)
+    {
+        memberID = id;
+        booksBorrowed = books;
+    }
+
+    int getMemberID()
+    {
+        return memberID;
+    }
+
+    int getBooksBorrowed()
+    {
+        return booksBorrowed;
+    }
+};
+
+// Second derived class
+class PremiumMember : public LibraryMember
+{
+private:
+    double membershipFee;
+
+public:
+    PremiumMember(string n, int id, int books, double fee)
+        : LibraryMember(n, id, books)
+    {
+        membershipFee = fee;
+    }
+
+    double getMembershipFee()
+    {
+        return membershipFee;
+    }
+};
+
+int main()
+{
+    PremiumMember member("Joshua", 101, 5, 2500.50);
+
+    cout << "Name: " << member.getName() << endl;
+    cout << "Member ID: " << member.getMemberID() << endl;
+    cout << "Books Borrowed: " << member.getBooksBorrowed() << endl;
+    cout << "Membership Fee: " << member.getMembershipFee() << endl;
+
+    return 0;
+}
