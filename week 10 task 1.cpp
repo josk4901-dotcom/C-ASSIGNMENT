095531b8cd3764f9b10f7dd9e529a915b963ea65
josk4901-dotcom <josk4901@gmail.com>
2026-07-12 19:41:45 +0300

    Add files via upload

---
+++ week 10 task 1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class Payment {
+public:
+    void processPayment(int amount) {
+        cout << "Cash Payment: " << amount << endl;
+    }
+
+    void processPayment(int amount, int fee) {
+        int total = amount + fee;
+        cout << "Card Payment: " << total << endl;
+    }
+
+    void processPayment(int amount, int fee, int discount) {
+        int total = amount + fee - discount;
+        cout << "Mobile Payment: " << total << endl;
+    }
+};
+
+int main() {
+    Payment p;
+
+    p.processPayment(1000);
+    p.processPayment(1000, 50);
+    p.processPayment(1000, 50, 100);
+
+    return 0;
+}
