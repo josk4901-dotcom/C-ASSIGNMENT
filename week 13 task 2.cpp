09220573611bb40190fccb714742f389680f1bd0
josk4901-dotcom <josk4901@gmail.com>
2026-07-19 23:04:30 +0300

    Add files via upload

---
+++ week 13 task 2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+struct Student {
+    char name[50];
+    int regNo;
+    float totalMarks;
+};
+
+int main() {
+    Student s;
+
+    ifstream file("results.dat", ios::binary);
+
+    if (!file) {
+        cout << "Error opening file!" << endl;
+        return 1;
+    }
+
+    while (file.read((char*)&s, sizeof(s))) {
+        cout << "Name: " << s.name << endl;
+        cout << "Registration Number: " << s.regNo << endl;
+        cout << "Total Marks: " << s.totalMarks << endl;
+        cout << "------------------------" << endl;
+    }
+
+    file.close();
+
+    return 0;
+}
