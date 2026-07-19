b1358b27ec335f5e3ada30f7cd4723f2db237b7e
josk4901-dotcom <josk4901@gmail.com>
2026-07-19 23:03:53 +0300

    Add files via upload

---
+++ week 13 task 1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main() {
+    ofstream file("books.txt", ios::app);
+
+    if (!file) {
+        cout << "Error opening file!" << endl;
+        return 1;
+    }
+
+    string title;
+
+    cout << "Enter book title: ";
+    getline(cin, title);
+
+    file << title << endl;
+    file.close();
+
+    cout << "Book title stored successfully." << endl;
+
+    return 0;
+}
