b30b8d80ebbb667e55b7452cb56fb52a1e139172
josk4901-dotcom <josk4901@gmail.com>
2026-07-19 23:01:37 +0300

    Add files via upload

---
+++ week 11 task 1.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Employee
+{
+private:
+    string employeeName;
+    int employeeID;
+    float basicSalary;
+    float netSalary;
+
+public:
+    void setEmployeeDetails()
+    {
+        cout << "Enter Employee Name: ";
+        getline(cin, employeeName);
+
+        cout << "Enter Employee ID: ";
+        cin >> employeeID;
+
+        cout << "Enter Basic Salary: ";
+        cin >> basicSalary;
+    }
+
+    void calculateSalary()
+    {
+        netSalary = basicSalary + (0.10 * basicSalary);
+    }
+
+    void displayEmployeeDetails()
+    {
+        cout << "\nEmployee Details" << endl;
+        cout << "Employee Name: " << employeeName << endl;
+        cout << "Employee ID: " << employeeID << endl;
+        cout << "Basic Salary: " << basicSalary << endl;
+        cout << "Net Salary: " << netSalary << endl;
+    }
+};
+
+int main()
+{
+    Employee emp;
+
+    emp.setEmployeeDetails();
+    emp.calculateSalary();
+    emp.displayEmployeeDetails();
+
+    return 0;
+}
