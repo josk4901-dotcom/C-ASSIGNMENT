35878e7f3d0e9946ae712b098767cd263d9b3c9a
josk4901-dotcom <josk4901@gmail.com>
2026-07-19 23:03:14 +0300

    Add files via upload

---
+++ week 12 task 2.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+class Shape
+{
+public:
+    virtual double calculateArea() = 0;
+};
+
+class Circle : public Shape
+{
+private:
+    double radius;
+
+public:
+    Circle(double r)
+    {
+        radius = r;
+    }
+
+    double calculateArea()
+    {
+        return 3.142 * radius * radius;
+    }
+};
+
+class Rectangle : public Shape
+{
+private:
+    double length;
+    double width;
+
+public:
+    Rectangle(double l, double w)
+    {
+        length = l;
+        width = w;
+    }
+
+    double calculateArea()
+    {
+        return length * width;
+    }
+};
+
+int main()
+{
+    Shape *shape;
+
+    Circle c(7);
+    Rectangle r(8, 5);
+
+    shape = &c;
+    cout << "Area of Circle: " << shape->calculateArea() << endl;
+
+    shape = &r;
+    cout << "Area of Rectangle: " << shape->calculateArea() << endl;
+
+    return 0;
+}
