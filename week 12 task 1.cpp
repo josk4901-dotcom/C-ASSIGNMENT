4aa74bb48c58c46274f5646a0476a2193bafaf82
josk4901-dotcom <josk4901@gmail.com>
2026-07-19 23:02:33 +0300

    Add files via upload

---
+++ week 12 task 1.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class MedicalStaff
+{
+protected:
+    string staffName;
+    int staffID;
+
+public:
+    MedicalStaff(string name, int id)
+    {
+        staffName = name;
+        staffID = id;
+    }
+
+    virtual double calculateSalary() = 0;
+
+    void displayDetails()
+    {
+        cout << "Staff Name: " << staffName << endl;
+        cout << "Staff ID: " << staffID << endl;
+    }
+};
+
+class Doctor : public MedicalStaff
+{
+private:
+    double basicSalary, allowance;
+
+public:
+    Doctor(string name, int id, double basic, double allow)
+        : MedicalStaff(name, id)
+    {
+        basicSalary = basic;
+        allowance = allow;
+    }
+
+    double calculateSalary()
+    {
+        return basicSalary + allowance;
+    }
+};
+
+class Nurse : public MedicalStaff
+{
+private:
+    double basicSalary, overtimePay;
+
+public:
+    Nurse(string name, int id, double basic, double overtime)
+        : MedicalStaff(name, id)
+    {
+        basicSalary = basic;
+        overtimePay = overtime;
+    }
+
+    double calculateSalary()
+    {
+        return basicSalary + overtimePay;
+    }
+};
+
+int main()
+{
+    MedicalStaff *staff;
+
+    Doctor doctor("John Doe", 101, 80000, 10000);
+    Nurse nurse("Mary Jane", 102, 50000, 5000);
+
+    staff = &doctor;
+    staff->displayDetails();
+    cout << "Salary: " << staff->calculateSalary() << endl;
+
+    cout << endl;
+
+    staff = &nurse;
+    staff->displayDetails();
+    cout << "Salary: " << staff->calculateSalary() << endl;
+
+    return 0;
+}
