5944e158c03abda8e2d0dbccfe4fbf8f724632ca
josk4901-dotcom <josk4901@gmail.com>
2026-07-19 23:05:07 +0300

    Add files via upload

---
+++ week 13 task 3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+    ifstream file("sales.txt");
+
+    if (!file) {
+        cout << "Error opening file!" << endl;
+        return 1;
+    }
+
+    double amount, total = 0;
+
+    while (file >> amount) {
+        total += amount;
+    }
+
+    cout << "Total Sales: " << total << endl;
+
+    file.close();
+
+    return 0;
+}
